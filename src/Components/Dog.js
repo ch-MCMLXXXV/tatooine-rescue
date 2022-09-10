813f5e2194f02008eb171d99d8d583eb14707e43
Lane <Kewllane@aol.com>
2022-09-10 11:36:16 -0500

    added dog component

---
+++ src/Components/Dog.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { fetchAllDogs } from "../api";
+
+const Dog = ({dogs}) => {
+    return (
+        <div className="dog">
+            <p><b>Dog #{dogs.id}</b></p>;
+            <p>Name: {dogs._name}</p>;
+            <p>Description: {dogs.description}</p>
+            <p>Breed: {dogs.breed}</p>
+            <p>Image: {dogs.image_url}</p>
+            <p>Adoption Fee: {dogs.adoption_fee}</p>
+        </div>
+    )
+}
+
+export default Dog
